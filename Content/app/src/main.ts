1492162fa4a3c6429f6d0056a2078da9ea46ea0d
Stualyttle <kilian.debock@student.arteveldehs.be>
2023-03-30 20:50:05 +0200

    Add content builder for config

--- Content/app/src/main.ts
+++ Content/app/src/main.ts
@@ -1,3 +1,9 @@
 import { generateModuleMarkdown } from './utils/generate-module-markdown';
+import { getModuleMarkdownContent } from './utils';
+import { generateModuleConfigs } from './utils/generate-module-configs';
 
 generateModuleMarkdown();
+generateModuleConfigs();
+
+const r = getModuleMarkdownContent('Activity.levels.cmd.you');
+console.log(r);
---
+++ Content/app/src/utils/generate-module-configs.ts
@@ -0,0 +1,38 @@
+import { spider } from './spider';
+import { saveFile } from './saveFile';
+import { buildConfig } from './module-configs';
+
+export function generateModuleConfigs() {
+  const moduleMarkdown = spider('../content/modules', buildConfig);
+
+  console.log(moduleMarkdown);
+
+  let content = '';
+
+  const builder = (obj: any, path: string) => {
+    Object.keys(obj).forEach((key) => {
+      if (typeof obj[key] === 'object') {
+        builder(obj[key], `${path}/${key}`);
+      } else {
+        const variable = (
+          obj[key][0].toUpperCase() + obj[key].slice(1)
+        ).replaceAll('.', '');
+        const globalVariable = (
+          path.replaceAll('../../content/modules/', '').split('/').join('_') +
+          '_' +
+          variable
+        )
+          .replaceAll('.', '')
+          .toUpperCase();
+        content += `export { ${variable} as __${globalVariable}__ } from '${path}';\n`;
+      }
+    });
+    content += '\n';
+  };
+
+  builder(moduleMarkdown, '../../content/modules');
+
+  content = content.trim();
+
+  saveFile('generated/ModuleConfigs.ts', content);
+}
