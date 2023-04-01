26c1cce7448570535931b1cd4c5126755085e7c5
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2023-04-01 16:17:37 +0200

    Syncronise For Dependabot Security Fix
    * Add Root imports
    
    * Add content builder for config
    
    * Remove logs & cleanup
    
    * Remove any & log
    
    * database diagram
    
    ---------
    
    Co-authored-by: Stualyttle <kilian.debock@student.arteveldehs.be>
    Co-authored-by: Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>

--- Content/app/src/utils/module-configs.ts
+++ Content/app/src/utils/module-configs.ts
@@ -1,1 +1,47 @@
-export function buildConfig(): void {}
+import { SpiderResults } from '../types/Spider';
+import path from 'path';
+import fs from 'fs';
+
+export function buildConfig({
+  files,
+  filePath,
+  fileName,
+  rawFilePath,
+}: SpiderResults): void {
+  // Check if the file is markdown.
+  if (rawFilePath.endsWith('config.ts')) {
+    // Resolve the file.
+    const file = path.resolve(filePath);
+
+    // Get the files content
+    const fileContents = fs
+      // Get contents
+      .readFileSync(file, 'utf8')
+      // Remove all Zero Width No-Break Space characters ( https://www.compart.com/en/unicode/U+FEFF )
+      .replaceAll('﻿', '');
+
+    const rawContent = fileContents.endsWith('\n')
+      ? // If the string ends with a new line, remove it
+        fileContents.slice(0, -1)
+      : // Otherwise, return the string as is
+        fileContents;
+
+    // get the selected groups back from regex
+    const regex = /\/\/ export:? ?(.*?)\nexport interface (.*) ({(.|\n)*?})/gm;
+
+    let m;
+    const result = [];
+
+    while ((m = regex.exec(rawContent)) !== null) {
+      // This is necessary to avoid infinite loops with zero-width matches
+      if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+      }
+
+      result.push(m[2]);
+    }
+
+    // Set variable to the main object
+    files[fileName] = result;
+  }
+}
