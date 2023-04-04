194296e44e80bed8dab066f3e5cd080208646ee6
Stualyttle <kilian.debock@student.arteveldehs.be>
2023-04-04 19:53:27 +0200

    Remove logs & update package.json

--- Client/src/utils/helpers/test.ts
+++ Client/src/utils/helpers/test.ts
@@ -1,13 +1,13 @@
 import { sleep } from './sleep';
 
 export async function test(...args) {
-  console.log('test');
+  // console.log('test');
   await sleep(100);
 
   // randomly throw an error
-  if (Math.random() < 0.3) {
-    console.log('test error');
-    throw new Error('test error');
-  }
-  console.log('test done');
+  // if (Math.random() < 0.3) {
+  //   console.log('test error');
+  //   throw new Error('test error');
+  // }
+  // console.log('test done');
 }
