f456b41f26d85c3475139602b90380d2e2b46548
Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>
2023-04-05 20:31:32 +0200

    resolve field for guild__translation (both all and one)

--- Api/src/guild-translation/guild-translation.service.ts
+++ Api/src/guild-translation/guild-translation.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateGuildTranslationInput } from './dto/create-guild-translation.input';
 import { UpdateGuildTranslationInput } from './dto/update-guild-translation.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { GuildTranslation } from './entities/guild-translation.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class GuildTranslationService {
+  constructor(
+    @InjectRepository(GuildTranslation)
+    private guildTranslationRepository: Repository<GuildTranslation>,
+  ) {}
+
   create(createGuildTranslationInput: CreateGuildTranslationInput) {
     return 'This action adds a new guildTranslation';
   }
 
-  findAll() {
-    return `This action returns all guildTranslation`;
+  findAll(): Promise<GuildTranslation[]> {
+    return this.guildTranslationRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} guildTranslation`;
+  findOne(id: number): Promise<GuildTranslation> {
+    return this.guildTranslationRepository.findOne({ where: { guild_id: id } });
   }
 
   update(id: number, updateGuildTranslationInput: UpdateGuildTranslationInput) {
