443fc26901df29bd0c6a0e28292308ea9af5bf6f
Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>
2023-04-05 14:09:30 +0200

    added relations to guild

--- Api/src/guild/entities/guild.entity.ts
+++ Api/src/guild/entities/guild.entity.ts
@@ -2,9 +2,14 @@ import { Field, Int, ObjectType } from '@nestjs/graphql';
 import {
   CreateDateColumn,
   Entity,
+  OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { GuildModuleLevel } from '../../guild-module-level/entities/guild-module-level.entity';
+import { GuildModuleQotd } from '../../guild-module-qotd/entities/guild-module-qotd.entity';
+
+let guildModuleLevel;
 
 @Entity()
 @ObjectType()
@@ -12,6 +17,14 @@ export class Guild {
   @PrimaryGeneratedColumn()
   @Field(() => Int)
   guild_id: number;
+
+  @OneToOne(() => GuildModuleLevel, { onDelete: 'CASCADE' })
+  @Field(() => guildModuleLevel)
+  guildModuleLevel: GuildModuleLevel;
+
+  @OneToOne(() => GuildModuleQotd, { onDelete: 'CASCADE' })
+  @Field(() => GuildModuleQotd)
+  guildModuleQotd: GuildModuleQotd;
 
   @CreateDateColumn()
   @Field(() => Date)
