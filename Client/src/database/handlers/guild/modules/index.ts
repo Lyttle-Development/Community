4bbf5b9bb15d0c9ec1409fd73c80cdbee7400315
Stualyttle <kilian.debock@student.arteveldehs.be>
2023-04-05 16:25:46 +0200

    Add guild/modules/voice-growth handlers

--- Client/src/database/handlers/guild/modules/index.ts
+++ Client/src/database/handlers/guild/modules/index.ts
@@ -2,3 +2,5 @@ export * from './CountToNumber';
 export * from './EasterEgg';
 export * from './Birthday';
 export * from './Level';
+export * from './VoiceGrowth';
+export * from './VoiceGrowthChild';
