53f76986092064c31fd854cf8fed6025750ba55a
Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>
2023-04-05 14:13:07 +0200

    finished relations to guild

--- Api/src/guild/entities/guild.entity.ts
+++ Api/src/guild/entities/guild.entity.ts
@@ -2,12 +2,15 @@ import { Field, Int, ObjectType } from '@nestjs/graphql';
 import {
   CreateDateColumn,
   Entity,
+  OneToMany,
   OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import { GuildModuleLevel } from '../../guild-module-level/entities/guild-module-level.entity';
 import { GuildModuleQotd } from '../../guild-module-qotd/entities/guild-module-qotd.entity';
+import { GuildMessage } from '../../guild-message/entities/guild-message.entity';
+import { GuildTranslation } from '../../guild-translation/entities/guild-translation.entity';
 
 let guildModuleLevel;
 
@@ -25,6 +28,20 @@ export class Guild {
   @OneToOne(() => GuildModuleQotd, { onDelete: 'CASCADE' })
   @Field(() => GuildModuleQotd)
   guildModuleQotd: GuildModuleQotd;
+
+  @OneToMany(
+    () => GuildMessage,
+    (guildMessage: GuildMessage) => guildMessage.guild_id,
+  )
+  @Field(() => [GuildMessage])
+  guildMessages: GuildMessage[];
+
+  @OneToMany(
+    () => GuildTranslation,
+    (guildTranslation: GuildTranslation) => guildTranslation.guild_id,
+  )
+  @Field(() => [GuildTranslation])
+  guildTranslations: GuildTranslation[];
 
   @CreateDateColumn()
   @Field(() => Date)
