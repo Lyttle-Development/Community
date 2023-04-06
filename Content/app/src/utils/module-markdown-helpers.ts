ef94885a9987ff4b1d9f27b438eda2645646cace
Stualyttle <kilian.debock@student.arteveldehs.be>
2023-04-06 12:30:44 +0200

    Fix eslint typescript errors/bugs

--- Content/app/src/utils/module-markdown-helpers.ts
+++ Content/app/src/utils/module-markdown-helpers.ts
@@ -5,8 +5,9 @@ import markdown from '../../generated/ModuleMarkdown';
 function getModuleMarkdownItem(path: string): ModuleMarkdownItem {
   const res = path.split('.').reduce((acc, cur) => acc[cur], markdown);
   if (!res || typeof res !== 'object') throw new Error('No object found.');
-  if (isVoid(res.content) || isVoid(res.documentation))
+  if (isVoid(res.content) || isVoid(res.documentation)) {
     throw new Error('No content or documentation found.');
+  }
   return {
     content: res.content as string,
     documentation: res.documentation as string,
@@ -14,11 +15,11 @@ function getModuleMarkdownItem(path: string): ModuleMarkdownItem {
 }
 
 export function getModuleMarkdownContent(path: string): string {
-  const markdown = getModuleMarkdownItem(path);
-  return markdown.content;
+  const _markdown = getModuleMarkdownItem(path);
+  return _markdown.content;
 }
 
 export function getModuleMarkdownDocumentation(path: string): string {
-  const markdown = getModuleMarkdownItem(path);
-  return markdown.documentation;
+  const _markdown = getModuleMarkdownItem(path);
+  return _markdown.documentation;
 }
