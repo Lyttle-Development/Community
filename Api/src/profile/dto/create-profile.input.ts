71d38a5937484057e2b6678e818dd6327fd4a18b
Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>
2023-04-11 15:06:10 +0200

    profile service

--- Api/src/profile/dto/create-profile.input.ts
+++ Api/src/profile/dto/create-profile.input.ts
@@ -1,13 +1,10 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
-import { Column } from 'typeorm';
 
 @InputType()
 export class CreateProfileInput {
-  @Column()
   @Field(() => Int)
   guild_id: number;
 
-  @Column()
   @Field(() => Int)
   tokens: number;
 }
