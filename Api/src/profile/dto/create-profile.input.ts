9d631edd62b1c6250565e59a6af2d7191899e017
Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>
2023-04-12 10:14:31 +0200

    fixed profile dto

--- Api/src/profile/dto/create-profile.input.ts
+++ Api/src/profile/dto/create-profile.input.ts
@@ -2,6 +2,12 @@ import { Field, InputType, Int } from '@nestjs/graphql';
 
 @InputType()
 export class CreateProfileInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int)
+  guild_id: number;
+
+  @Field(() => Int)
+  tokens: number;
+
+  @Field(() => Int)
+  tokens_used: number;
 }
