fcaeaa2a716e74f04ecdf6e12f072a8cbcd3a18b
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2023-04-14 12:35:14 +0200

    Add some more comments

--- Client/src/modules/Activity/dynamic-voice/check-channels.ts
+++ Client/src/modules/Activity/dynamic-voice/check-channels.ts
@@ -14,24 +14,36 @@ export async function checkDynamicChannels(
   oldState: VoiceState,
   newState: VoiceState,
 ) {
+  // Get guild id
   const { guildId } = guildMember;
 
+  // Check both channels
   await checkChannel(guildId, oldState.channelId);
+
+  // If the channel didn't change, break.
+  if (oldState.channelId === newState.channelId) return;
+
+  // Check the new channel
   await checkChannel(guildId, newState.channelId);
 }
 
 async function checkChannel(guildId, channelId) {
+  // If no channel, break.
   if (!channelId) return;
 
+  // Check if the channel is a child channel
   const db_GuildModuleVoiceGrowthChild =
     await findSingleGuildModuleVoiceGrowthChild(guildId, channelId);
 
+  // If it is a child channel, get the master channel
   if (
     db_GuildModuleVoiceGrowthChild &&
     db_GuildModuleVoiceGrowthChild.master_id
   ) {
+    // Get the master channel id
     channelId = db_GuildModuleVoiceGrowthChild.master_id.toString();
   }
 
+  // Trigger the event, with the master channel id
   await triggerDynamicVoiceEvent(guildId, channelId);
 }
--- Client/src/modules/Activity/dynamic-voice/trigger-dynamic-voice-event.ts
+++ Client/src/modules/Activity/dynamic-voice/trigger-dynamic-voice-event.ts
@@ -15,15 +15,21 @@ export async function triggerDynamicVoiceEvent(
   guildId: string,
   channelId: string,
 ) {
+  // If the channel is already being checked, wait 1 second and try again
   if (channelsBeingChecked[channelId]) {
+    // Wait 1 second
     await sleep(1000);
+    // Try again
     return triggerDynamicVoiceEvent(guildId, channelId);
   }
+  // Add the channel to the being checked cache
   channelsBeingChecked[channelId] = true;
 
   // Trigger the event
   const actionWasNeeded = await fireDynamicVoiceEvent(guildId, channelId);
+  // If no action was needed, return (thus everything is as it should be)
   if (!actionWasNeeded) return;
+  // An action was needed, so wait 5 seconds and check again.
 
   // Wait for 5 seconds
   await sleep(5 * 1000);
@@ -41,32 +47,42 @@ async function fireDynamicVoiceEvent(
   guildId: string,
   channelId: string,
 ): Promise<boolean> {
+  // Get the database entry for the channel
   const db_GuildModuleVoiceGrowth = await findSingleGuildModuleVoiceGrowth(
     guildId,
     channelId,
   );
 
-  // If not enabled, return
+  // If not enabled, ot it's a manual channel, stop.
   if (!db_GuildModuleVoiceGrowth?.enabled || db_GuildModuleVoiceGrowth.manual) {
     // Remove the channel from the being checked cache
     delete channelsBeingChecked[channelId];
+    // Return as no action was needed
     return false;
   }
 
+  // Get the master channel
   const master = db_GuildModuleVoiceGrowth;
+  // Get the child channels
   const childs = db_GuildModuleVoiceGrowth.childs;
 
+  // Get master channel id
   const masterId = master.channel_id.toString();
+  // Get all child channel ids
   const childIds = childs.map((x) => x.channel_id.toString());
   // Get all channel ids, including the master channel
   const channelIds = [masterId, ...childIds];
 
+  // Get all channels
   const channels = channelIds.map((x) => client.channels.cache.get(x)) || [];
+
+  // Get all voice channels
   const voiceChannels =
     channels
       .filter((x) => x.type === ChannelType.GuildVoice)
       .map((x) => x as VoiceChannel) || [];
 
+  // Get all empty channels
   const emptyChannels = voiceChannels?.filter((x) => x.members.size < 1) || [];
 
   // If there are too many empty channels, delete all but one
@@ -93,7 +109,7 @@ async function fireDynamicVoiceEvent(
       }
     });
 
-    // Stop the function
+    // Stop, and return that an action was needed
     return true;
   }
 
@@ -131,11 +147,13 @@ async function fireDynamicVoiceEvent(
       delete channelsBeingChecked[masterId];
     });
 
-    // Stop the function
+    // Stop, and return that an action was needed
     return true;
   }
 
   // Remove the channel from the being checked cache
   delete channelsBeingChecked[masterId];
+
+  // Return that no action was needed
   return false;
 }
