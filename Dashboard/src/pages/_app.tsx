b32a3a994a49277d041f70ce65d48aca36b4b189
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2023-05-13 18:48:59 +0200

    Mute eslint error

--- Dashboard/src/pages/_app.tsx
+++ Dashboard/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 
+// eslint-disable-next-line @typescript-eslint/ban-types
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
