325f91fc5d4f544ffc2062a7e502f8e961ad8f0d
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2023-05-18 16:29:20 +0200

    Remove duplicate in-exports

--- Client/src/modules/Activity/index.ts
+++ Client/src/modules/Activity/index.ts
@@ -1,5 +1,3 @@
 export * from './levels';
-export * from '../Communication/dynamic-voice';
 export * from './audit-logs';
-export * from '../Communication/voice-topics';
 export * from './interactions';
