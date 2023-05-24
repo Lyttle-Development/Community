80211f49839e82d51746fec765809b5b5b3ed521
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2023-05-24 18:31:44 +0200

    Add side menu

--- Dashboard/src/components/main-nav/main-nav.tsx
+++ Dashboard/src/components/main-nav/main-nav.tsx
@@ -1,23 +1,53 @@
 import styles from './main-nav.module.scss';
+import { MainNavItem } from '@lyttledev-dashboard/components/main-nav-item';
+import { Component } from '@lyttledev-dashboard/components';
+import Link from 'next/link';
+import { useApp } from '@lyttledev-dashboard/contexts/App.context';
 
 export interface MainNavProps {
   open: boolean;
 }
 
 export function MainNav({ open }: MainNavProps) {
+  const app = useApp();
+  const selectedGuildId = app?.selectedGuildId ?? null;
   const prefix = 'main-nav';
   const openClass = open
     ? `${prefix} ${prefix}--open`
     : `${prefix} ${prefix}--closed`;
+
+  const signOut = () => {
+    window.alert('sign out');
+  };
+
   return (
     <aside className={`${openClass} ${styles['main-menu']}`}>
+      <Link href="/">
+        <Component.Logo className={styles.logo} />
+      </Link>
       <nav>
         <ul>
-          <li>Dashboard</li>
-          <li>Users</li>
-          <li>Settings</li>
+          <MainNavItem href={'/'}>Home</MainNavItem>
+          <MainNavItem href={'/dashboard'}>Servers</MainNavItem>
+          {selectedGuildId && (
+            <>
+              <MainNavItem href={`/dashboard/${selectedGuildId}/modules`}>
+                Modules
+              </MainNavItem>
+              <MainNavItem href={`/dashboard/${selectedGuildId}/statistics`}>
+                Statistics
+              </MainNavItem>
+              <MainNavItem href={`/dashboard/${selectedGuildId}/messages`}>
+                Messages
+              </MainNavItem>
+            </>
+          )}
+          <MainNavItem href={'/profile'}>Profile</MainNavItem>
         </ul>
       </nav>
+      <ul className={`${styles['main-menu__footer']}`}>
+        <MainNavItem onClick={signOut}>Logout</MainNavItem>
+      </ul>
     </aside>
   );
 }
