6be133a215ad9316fbccbbd3c30c9c634e808684
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2023-06-16 16:05:13 +0200

    Add check to prevent empty data && add comments

--- Client/src/modules/Administration/stats/reset-caches.ts
+++ Client/src/modules/Administration/stats/reset-caches.ts
@@ -6,15 +6,28 @@ import {
 } from '../../../database/handlers';
 
 export async function resetCaches() {
+  // Get today's day as int
   const todayInt = new Date().getDay();
+
+  // Get all guild stats
   const db_GuildStats = await getAllGuildStatsCaches();
 
   for (const { guild_id, key, value, value_int } of db_GuildStats) {
+    // Prevents resetting the stats if the value is 0
+    if (value_int === 0) continue;
+
+    // get the guild id as a string
     const guildId = guild_id.toString();
+
+    // get the group key
     const { group_key } = await findOneGuildStatsByGuildAndKey(guildId, key);
+
+    // set cached result as real result
     await setGuildStat(guildId, key, todayInt, value, value_int, group_key);
+    // reset cached result back to 0
     await setGuildStat(guildId, key, -2, null, 0);
   }
 
+  // remove outdated stats (older than 7 days)
   await removeOutdatedGuildStats();
 }
