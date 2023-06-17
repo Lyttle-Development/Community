dd644813fe56a6ad07123c6600b33fac1fca6232
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2023-06-17 15:44:37 +0200

    Add comments

--- Dashboard/src/components/review/review.builder.ts
+++ Dashboard/src/components/review/review.builder.ts
@@ -28,6 +28,7 @@ interface reviewBuilderInfo {
   [key: string]: {
     query: ReviewBuilderMutationOptions;
     variable: string;
+    booleanNull?: boolean;
   };
 }
 
@@ -36,7 +37,7 @@ export interface ReviewBuilderQuery {
     name: string;
     variablesName: string;
     variables: {
-      [key: string]: string | boolean | number;
+      [key: string]: string | boolean | number | null;
     };
   };
 }
@@ -105,10 +106,12 @@ export const reviewBuilderInfo: reviewBuilderInfo = {
   [changeKeys.moduleLevelsAnnouncement.key]: {
     query: reviewBuilderMutations.updateGuildModuleLevel,
     variable: 'announcementChannelId',
+    booleanNull: true,
   },
   [changeKeys.moduleLevelsLeaderboard.key]: {
     query: reviewBuilderMutations.updateGuildModuleLevel,
     variable: 'leaderboardChannelId',
+    booleanNull: true,
   },
   [changeKeys.moduleLevelsNickname.key]: {
     query: reviewBuilderMutations.updateGuildModuleLevel,
@@ -125,6 +128,7 @@ export const reviewBuilderInfo: reviewBuilderInfo = {
   [changeKeys.moduleBirthdayChannel.key]: {
     query: reviewBuilderMutations.updateGuildModuleBirthday,
     variable: 'channelId',
+    booleanNull: true,
   },
   [changeKeys.moduleBirthdayText.key]: {
     query: reviewBuilderMutations.createGuildTranslation,
@@ -138,26 +142,44 @@ export function reviewBuilder(
   guildId: string | null,
   changes: [string, ChangeObject][],
 ) {
+  // Check for missing keys
   const missingKeys = changeKeysValuesArray.filter(
+    // Keys from changeKeys is inside reviewBuilderInfoKeys? If not, it's missing
     ({ key }) => !reviewBuilderInfoKeys.includes(key),
   );
+  // Check if there's any missing keys
   if (missingKeys.length > 0) {
+    // Get the keys and join them with a comma
     const missingKeysStr = missingKeys.map((key) => key.key).join(', ');
+    // Throw an error with the missing keys
     throw new Error('Missing change keys: ' + missingKeysStr);
   }
 
+  // Check if there's any changes
   if (!guildId) return null;
 
+  // Create the query object
   const query: ReviewBuilderQuery = {};
 
-  for (const [name, change] of changes) {
-    const builderInfo = reviewBuilderInfo[name];
+  // Loop through the changes
+  for (const [changeKey, change] of changes) {
+    // Get the builder info
+    const builderInfo = reviewBuilderInfo[changeKey];
+    // Get the query name
     let queryName = builderInfo.query.name;
+
+    // If the query is a translation, add the translation suffix
     if (builderInfo.query.translation) {
-      queryName = queryName + '__' + 'translation//' + name;
+      queryName = queryName + '__' + 'translation//' + changeKey;
     }
+
+    // If the query requires a guild id, add it
     const value = change.current;
+
+    // If the value is null, continue the loop.
     if (value === null) continue;
+
+    // if no object is set yet, create it
     if (!query[queryName]?.name) {
       query[queryName] = {
         name: queryName,
@@ -167,44 +189,81 @@ export function reviewBuilder(
         },
       };
     } else {
+      // If the object is set, add the variable
       query[queryName].variables[builderInfo.variable] = value;
     }
 
+    // If the query is a translation, add the key
     if (builderInfo.query.translation) {
+      // Find the key
       const key = changeKeysValuesArray.find(
-        (c) => c.key === name,
+        (c) => c.key === changeKey,
       )?.translationKey;
+      // If a key is found, add it to the variables
       if (key) query[queryName].variables.key = key;
     }
 
+    // Modifications when booleanNull is true
+    if (builderInfo?.booleanNull) {
+      // Explicit false, not null, undefined or etc...
+      if (value === false) {
+        // Set the variable to null
+        query[queryName].variables[builderInfo.variable] = null;
+      }
+      // Same here, but true
+      if (value === true) {
+        // Delete the variable
+        delete query[queryName].variables[builderInfo.variable];
+      }
+    }
+
+    // Add the guild id if the query requires it
     if (builderInfo.query.requiresGuildId) {
       query[queryName].variables.guildId = guildId;
     }
   }
 
+  // Build the mutation and return it.
   return buildQuery(query);
 }
 
-function buildQuery(query: ReviewBuilderQuery) {
-  if (Object.keys(query).length < 1) return null;
+function buildQuery(queries: ReviewBuilderQuery) {
+  // Check if there's any queries
+  if (Object.keys(queries).length < 1) return null;
+
+  // Build the query string
   let queryString = 'mutation {';
 
-  for (const key in query) {
-    const q = query[key];
-    const variables = q.variables || {};
+  // Loop through the queries
+  for (const key in queries) {
+    // Get the query
+    const query = queries[key];
+    // Get the variables
+    const variables = query.variables || {};
+    // Get the variables as an array
     const variablesObj = Object.entries(variables);
+    // Get the variables as a string
     const variablesString = variablesObj
-      .map((v) => {
-        const value = v[1];
-        if (typeof value === 'string') return `${v[0]}: "${value}"`;
-        return `${v[0]}: ${value}`;
+      // Loop through the variables
+      .map(([name, value]) => {
+        // If the value is a string, add quotes
+        if (typeof value === 'string') return `${name}: "${value}"`;
+        // otherwise, just return the value
+        return `${name}: ${value}`;
       })
+      // Join the variables with a comma
       .join(', ');
-    const name = q.name.split('__')[0];
-    queryString += `\n  ${name}(${q.variablesName}: { ${variablesString} }) { __typename }`;
+
+    // Get the real name, without the added suffix
+    const name = query.name.split('__')[0];
+
+    // Add the query to the query string
+    queryString += `\n  ${name}(${query.variablesName}: { ${variablesString} }) { __typename }`;
   }
 
+  // Close the query string
   queryString += '\n}';
 
+  // Return the query string
   return queryString;
 }
