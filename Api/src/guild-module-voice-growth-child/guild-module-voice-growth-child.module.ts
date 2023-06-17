dc6deae9bb82e419422c147bb790a34a32cfbae4
Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>
2023-06-17 18:14:09 +0200

    voice growth child

--- Api/src/guild-module-voice-growth-child/guild-module-voice-growth-child.module.ts
+++ Api/src/guild-module-voice-growth-child/guild-module-voice-growth-child.module.ts
@@ -1,11 +1,19 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { GuildModuleVoiceGrowthChildService } from './guild-module-voice-growth-child.service';
 import { GuildModuleVoiceGrowthChildResolver } from './guild-module-voice-growth-child.resolver';
+import { GuildModuleVoiceGrowthService } from '../guild-module-voice-growth/guild-module-voice-growth.service';
+import { GuildModuleVoiceGrowthChild } from './entities/guild-module-voice-growth-child.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([GuildModuleVoiceGrowthChild]),
+    forwardRef(() => GuildModuleVoiceGrowthService),
+  ],
   providers: [
     GuildModuleVoiceGrowthChildResolver,
     GuildModuleVoiceGrowthChildService,
   ],
+  exports: [GuildModuleVoiceGrowthChildService],
 })
 export class GuildModuleVoiceGrowthChildModule {}
--- Api/src/guild-module-voice-growth-child/guild-module-voice-growth-child.service.ts
+++ Api/src/guild-module-voice-growth-child/guild-module-voice-growth-child.service.ts
@@ -1,31 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { CreateGuildModuleVoiceGrowthChildInput } from './dto/create-guild-module-voice-growth-child.input';
 import { UpdateGuildModuleVoiceGrowthChildInput } from './dto/update-guild-module-voice-growth-child.input';
+import { Repository } from 'typeorm';
+import { GuildModuleVoiceGrowthChild } from './entities/guild-module-voice-growth-child.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { GuildModuleVoiceGrowthService } from '../guild-module-voice-growth/guild-module-voice-growth.service';
+import { GuildModuleVoiceGrowth } from '../guild-module-voice-growth/entities/guild-module-voice-growth.entity';
 
 @Injectable()
 export class GuildModuleVoiceGrowthChildService {
-  create(
+  constructor(
+    @InjectRepository(GuildModuleVoiceGrowthChild)
+    private guildModuleVoiceGrowthChildRepository: Repository<GuildModuleVoiceGrowthChild>,
+    @Inject(forwardRef(() => GuildModuleVoiceGrowthService))
+    private guildModuleVoiceGrowthService: GuildModuleVoiceGrowthService,
+  ) {}
+
+  async create(
     createGuildModuleVoiceGrowthChildInput: CreateGuildModuleVoiceGrowthChildInput,
-  ) {
-    return 'This action adds a new guildModuleVoiceGrowthChild';
+  ): Promise<GuildModuleVoiceGrowthChild> {
+    return this.guildModuleVoiceGrowthChildRepository.create(
+      createGuildModuleVoiceGrowthChildInput,
+    );
   }
 
-  findAll() {
-    return `This action returns all guildModuleVoiceGrowthChild`;
+  findAll(): Promise<GuildModuleVoiceGrowthChild[]> {
+    return this.guildModuleVoiceGrowthChildRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} guildModuleVoiceGrowthChild`;
+  findOne(
+    guildId: string,
+    channelId: string,
+  ): Promise<GuildModuleVoiceGrowthChild> {
+    return this.guildModuleVoiceGrowthChildRepository.findOne({
+      where: { guildId: guildId, channelId: channelId },
+    });
   }
 
-  update(
-    id: number,
+  getGuildModuleVoiceGrowth(guildId: string): Promise<GuildModuleVoiceGrowth> {
+    return this.guildModuleVoiceGrowthService.findOne(guildId);
+  }
+
+  async update(
+    guildId: string,
+    channelId: string,
     updateGuildModuleVoiceGrowthChildInput: UpdateGuildModuleVoiceGrowthChildInput,
-  ) {
-    return `This action updates a #${id} guildModuleVoiceGrowthChild`;
+  ): Promise<GuildModuleVoiceGrowthChild> | null {
+    const guildModuleVoiceGrowthChild: GuildModuleVoiceGrowthChild =
+      await this.guildModuleVoiceGrowthChildRepository.findOne({
+        where: { guildId: guildId, channelId: channelId },
+      });
+    if (GuildModuleVoiceGrowthChild) {
+      return this.guildModuleVoiceGrowthChildRepository.save({
+        ...guildModuleVoiceGrowthChild,
+        ...updateGuildModuleVoiceGrowthChildInput,
+      });
+    }
+    return null;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} guildModuleVoiceGrowthChild`;
+  async remove(
+    guildId: string,
+    channelId: string,
+  ): Promise<GuildModuleVoiceGrowthChild> | null {
+    const guildModuleVoiceGrowthChild: GuildModuleVoiceGrowthChild =
+      await this.guildModuleVoiceGrowthChildRepository.findOne({
+        where: { guildId: guildId, channelId: channelId },
+      });
+    if (guildModuleVoiceGrowthChild) {
+      return this.guildModuleVoiceGrowthChildRepository.remove(
+        guildModuleVoiceGrowthChild,
+      );
+    }
+    return null;
   }
 }
