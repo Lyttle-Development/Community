c76d76bd68c4545ba585d2060b1ffb7e245e7237
Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>
2023-06-17 15:22:51 +0200

    fixed import error

--- Api/src/openai/openai.module.ts
+++ Api/src/openai/openai.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { OpenaiService } from './openai.service';
 import { OpenaiResolver } from './openai.resolver';
+import { GuildStatResolvedModule } from '../guild-stat-resolved/guild-stat-resolved.module';
+import { DiscordModule } from '../discord/discord.module';
 
 @Module({
+  imports: [
+    forwardRef(() => GuildStatResolvedModule),
+    forwardRef(() => DiscordModule),
+  ],
   providers: [OpenaiResolver, OpenaiService],
   exports: [OpenaiService],
 })
--- Api/src/openai/openai.service.ts
+++ Api/src/openai/openai.service.ts
@@ -1,17 +1,61 @@
-import { Injectable } from "@nestjs/common";
-//
-// const { Configuration, OpenAIApi } = require('openai');
-//
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { GuildStatResolvedService } from '../guild-stat-resolved/guild-stat-resolved.service';
+import { DiscordService } from '../discord/discord.service';
+
+const { Configuration, OpenAIApi } = require('openai');
+
 @Injectable()
 export class OpenaiService {
-  //   private openai: any;
-  //
-  //   constructor(
-  //     @Inject(forwardRef(() => ))
-  //   ) {
-  //     const configuration = new Configuration({
-  //       apiKey: process.env.OPENAI_API_KEY
-  //     });
-  //     this.openai = new OpenAIApi(configuration);
-  //   }
+  private openai: any;
+
+  constructor(
+    @Inject(forwardRef(() => GuildStatResolvedService))
+    private guildStatResolvedService: GuildStatResolvedService,
+    @Inject(forwardRef(() => DiscordService))
+    private discordService: DiscordService,
+  ) {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    this.openai = new OpenAIApi(configuration);
+  }
+
+  async create(prompt: string, maxTokens: number = 0.25): Promise<string> {
+    const result = await this.openai.complete({
+      engine: 'davinci',
+      prompt,
+      maxTokens,
+      temperature: 0.9,
+      topP: 1,
+      presencePenalty: 0,
+      frequencyPenalty: 0,
+      bestOf: 1,
+      n: 1,
+      stream: false,
+      stop: ['\n'],
+    });
+
+    return result.data.choices[0].text;
+  }
+
+  async createWithGuildStat(guildId: string): Promise<string> {
+    const guildStatStaffMembers: number =
+      await this.guildStatResolvedService.getStaffMembers(guildId);
+    const guildStatBots: number = await this.guildStatResolvedService.getBots(
+      guildId,
+    );
+    const guildStatChannels: number =
+      await this.guildStatResolvedService.getChannels(guildId);
+    const guildStatTextChannels: number =
+      await this.guildStatResolvedService.getTextChannels(guildId);
+    const guildStatVoiceChannels: number =
+      await this.guildStatResolvedService.getVoiceChannels(guildId);
+    const guildStatTextChannelsMessages =
+      await this.guildStatResolvedService.getTextChannelsMessages(guildId);
+    const guildStatVoiceChannelsCallTime =
+      await this.guildStatResolvedService.getVoiceChannelsCallTime(guildId);
+    const prompt = ``;
+    const result = await this.create(prompt);
+    return result;
+  }
 }
