40c3196e310e8473f177f0d9e1e513646541e240
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2024-03-07 21:17:14 +0100

    Add animation to stats card.

--- Dashboard/src/components/stats-card/stats-card.tsx
+++ Dashboard/src/components/stats-card/stats-card.tsx
@@ -1,5 +1,6 @@
 import styles from './stats-card.module.scss';
 import { formatNumber } from '@lyttledev-dashboard/utils';
+import { useEffect, useState } from 'react';
 
 export enum StatsCardColors {
   Purple = 'purple',
@@ -21,13 +22,52 @@ export function StatsCard({
   value,
   change = 0,
   changeNote,
-  total,
+  total = 0,
   color = StatsCardColors.Purple,
 }: StatsCardProps) {
+  const [currentTotal, setCurrentTotal] = useState(total);
+  const [degrees, setDegrees] = useState(0);
+  const [rotate, setRotate] = useState(false);
   const changeUnit = change ? (change < 0 ? '- ' : '+ ') : '';
   const changeValue = change < 0 ? change * -1 : change;
+
   // round to 5 , 10 ,15 20
-  const roundedTotal = total ? Math.round(total / 5) * 5 : 0;
+  // const roundedTotal = total ? Math.round(total / 5) * 5 : 0;
+  const setTotal = () => {
+    if (currentTotal !== total) {
+      if (currentTotal < total) {
+        setCurrentTotal(currentTotal + 1);
+      }
+      if (currentTotal > total) {
+        setCurrentTotal(currentTotal - 1);
+      }
+    }
+
+    if (total > 0) {
+      let totalDegrees = (currentTotal / 100) * 360 + 270;
+      totalDegrees = totalDegrees > 360 ? totalDegrees - 360 : totalDegrees;
+
+      setDegrees(totalDegrees);
+      if (currentTotal > 50) {
+        setRotate(true);
+      } else {
+        setRotate(false);
+      }
+    } else {
+      setDegrees(270);
+      setRotate(false);
+    }
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(setTotal, 10);
+    return () => clearTimeout(timeout);
+  }, [total, currentTotal]);
+
+  useEffect(() => {
+    setTotal();
+  }, []);
+
   return (
     <article className={`${styles.card} ${color && styles[`card--${color}`]}`}>
       <section className={styles.stats}>
@@ -43,8 +83,22 @@ export function StatsCard({
           )}
         </h2>
       </section>
-      <p className={`${styles.total} ${styles[`total--${roundedTotal}`]}`}>
-        <span className={styles['total__value']}>{total}%</span>
+      <p className={styles.total}>
+        <span className={styles['progress-bar__container']}>
+          <span
+            className={styles['progress-bar']}
+            style={{
+              transform: rotate ? 'rotate(180deg)' : 'rotate(0deg)',
+              background: `linear-gradient(${degrees}deg, ${
+                rotate ? 'white' : 'black'
+              } 50%, transparent 0),
+      linear-gradient(${
+        rotate ? 'to right' : 'to left'
+      }, white 50%, transparent 0)`,
+            }}
+          ></span>
+        </span>
+        <span className={styles['total__value']}>{currentTotal}%</span>
       </p>
     </article>
   );
