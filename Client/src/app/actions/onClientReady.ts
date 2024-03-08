31503852463efd7cd6d29e28a9246a8396a1e374
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2024-03-08 20:37:34 +0100

    Trigger daly stats on startup

--- Client/src/app/actions/onClientReady.ts
+++ Client/src/app/actions/onClientReady.ts
@@ -3,6 +3,7 @@ import { Client } from 'discord.js';
 import { deployCommands } from '../../utils/deploy-commands';
 import { executor, startupStatsQueue } from '../../utils';
 import { startup } from '../../utils/helpers/startup';
+import { triggerDailyStats } from '../../modules';
 
 // This file's prefix
 const prefix: string = actionPrefix + 'onClientReady.';
@@ -11,9 +12,10 @@ const prefix: string = actionPrefix + 'onClientReady.';
 export async function onClientReady(client: Client): Promise<void> {
   // All actions that should be executed
   const actions: Promise<() => void>[] = [
+    executor(prefix + 'startup', startup, client),
     executor(prefix + 'deployCommands', deployCommands),
     executor(prefix + 'startupStatsQueue', startupStatsQueue),
-    executor(prefix + 'startup', startup, client),
+    executor(prefix + 'triggerDailyStats', triggerDailyStats),
   ];
 
   // If no actions, return
