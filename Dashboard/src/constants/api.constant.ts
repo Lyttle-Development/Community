5bda9d11770545a4f222430985d6dde0bcaf7c89
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2024-04-13 16:11:12 +0200

    Reupdate api constants.

--- Dashboard/src/constants/api.constant.ts
+++ Dashboard/src/constants/api.constant.ts
@@ -1,23 +1,5 @@
-const env = process.env;
-
-export const graphQlUrl =
-  env.NEXT_PUBLIC_GRAPHQL_URL ||
-  process.env.NEXT_PUBLIC_GRAPHQL_URL ||
-  'http://localhost:3000/graphql';
-
-export const loginUrl =
-  env.NEXT_PUBLIC_LOGIN_URL ||
-  process.env.NEXT_PUBLIC_LOGIN_URL ||
-  'http://localhost:3000/auth/login';
-export const logoutUrl =
-  env.NEXT_PUBLIC_LOGOUT_URL ||
-  process.env.NEXT_PUBLIC_LOGOUT_URL ||
-  'http://localhost:3000/auth/logout';
-export const loginReturnUrl =
-  env.NEXT_PUBLIC_LOGIN_URL_RETURN ||
-  process.env.NEXT_PUBLIC_LOGIN_URL_RETURN ||
-  'http://localhost:3000/auth/discord';
-export const checkLoginUrl =
-  env.NEXT_PUBLIC_CHECK_URL ||
-  process.env.NEXT_PUBLIC_CHECK_URL ||
-  'http://localhost:3000/';
+export const graphQlUrl = process.env.NEXT_PUBLIC_GRAPHQL_URL;
+export const loginUrl = process.env.NEXT_PUBLIC_LOGIN_URL;
+export const logoutUrl = process.env.NEXT_PUBLIC_LOGOUT_URL;
+export const loginReturnUrl = process.env.NEXT_PUBLIC_LOGIN_URL_RETURN;
+export const checkLoginUrl = process.env.NEXT_PUBLIC_CHECK_URL;
